d576558a90cffc6c8ce8a000dbfe4be8f69505e6
gabriel250206 <19050@portalcedi.edu.mx>
2025-04-29 10:48:31 -0600

    instalaciones para el mapa, pero no se ve nada

--- proyectoFINAL/src/app/gym-cerca/gym-cerca.page.ts
+++ proyectoFINAL/src/app/gym-cerca/gym-cerca.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Geolocation } from '@capacitor/geolocation';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-gym-cerca',
@@ -13,8 +15,33 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
 export class GymCercaPage implements OnInit {
 
   constructor() { }
+  map: L.Map = {} as L.Map;
 
   ngOnInit() {
+    this.loadMap();
+  }
+  async getUserLocation() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    console.log('Current position:', coordinates);
+  }
+
+
+
+
+  async loadMap() {
+    const coordinates = await Geolocation.getCurrentPosition();
+    const lat = coordinates.coords.latitude;
+    const lon = coordinates.coords.longitude;
+  
+    this.map = L.map('map').setView([lat, lon], 15);
+  
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap contributors'
+    }).addTo(this.map);
+  
+    L.marker([lat, lon]).addTo(this.map)
+      .bindPopup('Estás aquí')
+      .openPopup();
   }
 
 }
