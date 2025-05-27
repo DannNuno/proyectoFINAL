9c240246f4bb60f97acb8affb60ee98274dcb9d6
gabriel250206 <19050@portalcedi.edu.mx>
2025-05-27 10:45:50 -0600

    index funcional sin todo el css

--- proyectoFINAL/src/app/index/index.page.ts
+++ proyectoFINAL/src/app/index/index.page.ts
@@ -1,20 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
-
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class IndexPage implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+  navigateToEjercicios(){
+    this.router.navigateByUrl("ejercicios");
+  }
+   navigateToMapa(){
+    this.router.navigateByUrl("gym-cerca");
+  }
+   navigateToComentarios(){
+    this.router.navigateByUrl("comentarios");
+  }
+
+
 
 }
