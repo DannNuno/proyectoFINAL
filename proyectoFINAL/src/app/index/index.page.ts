43bfa33e82ab35bab3415defa6e5cec1e4ce529a
gabriel250206 <19050@portalcedi.edu.mx>
2025-05-28 22:29:09 -0600

    animación y degradado en botones

--- proyectoFINAL/src/app/index/index.page.ts
+++ proyectoFINAL/src/app/index/index.page.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit,ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButton, IonButtons } from '@ionic/angular/standalone';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-index',
   templateUrl: './index.page.html',
   styleUrls: ['./index.page.scss'],
   standalone: true,
-  imports: [IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class IndexPage implements OnInit {
 
-  constructor(private router:Router) { }
+  constructor(private router:Router, private elRef:ElementRef) { }
+
+
+
+
+
+
+
+
 
   ngOnInit() {
   }
@@ -25,6 +33,10 @@ export class IndexPage implements OnInit {
    navigateToComentarios(){
     this.router.navigateByUrl("comentarios");
   }
+
+
+
+
 
 
 
